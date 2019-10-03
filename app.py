d73a4337c14fe6821b8b44e2b9b4d385e4705694
alexbarksdale <ale.barksdale@gmail.com>
2019-10-03 15:25:16 -0700

    Home page completed

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template('home.html')
+    
+
+@app.route('/market')
+def market_page():
+    return render_template('market.html')
