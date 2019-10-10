aa6d2265e45440e0d368fd7e6c37575bce890b00
alexbarksdale <ale.barksdale@gmail.com>
2019-10-09 22:58:09 -0700

    Heroku changes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import bcrypt
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # MONGO_URI = os.environ.get('MONGO_URI')
 
-host = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/Muzica')
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Muzica')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_database('muzica_db')
 listings = db.muzica_listings
